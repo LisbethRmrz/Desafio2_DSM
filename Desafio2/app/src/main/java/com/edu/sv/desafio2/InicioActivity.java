ce8cdcba2b10dad4da9ff4f9de0510974c40752b
LisbethRmrz <lisbethrmrz98@gmail.com>
2021-04-06 22:57:51 -0600

    Inicio de sesión, splash e icono

---
+++ Desafio2/app/src/main/java/com/edu/sv/desafio2/InicioActivity.java
@@ -0,0 +1,14 @@
+package com.edu.sv.desafio2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InicioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio);
+    }
+}
---
+++ Desafio2/app/src/main/java/com/edu/sv/desafio2/LoginActivity.java
@@ -0,0 +1,116 @@
+package com.edu.sv.desafio2;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import android.text.TextUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
+public class LoginActivity extends AppCompatActivity {
+
+    private TextView link;
+    public static final int GOOGLE_SIGN_IN_API=777;
+    private EditText emailTV, passwordTV;
+    private Button loginBtn;
+    private ProgressBar progressBar;
+
+    private Button signInButton;
+    private FirebaseAuth mAuth;
+
+    private GoogleApiClient googleApiClient;
+    public static final int SIGN_IN_CODE=777;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+        //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+        mAuth = FirebaseAuth.getInstance();
+        initializeUI();
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginUserAccount();
+            }
+        });
+        //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+        link = (TextView) findViewById(R.id.forget_password_link);
+
+        link.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, ForgotPasswordActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::.
+
+    }
+
+    private void loginUserAccount() {
+        progressBar.setVisibility(View.VISIBLE);
+
+        String email, password;
+        email = emailTV.getText().toString();
+        password = passwordTV.getText().toString();
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(), "Porfavor, ingrese su correo", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(), "Porfavor, ingrese su contraseña", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "Bienvenid@!", Toast.LENGTH_LONG).show();
+                            progressBar.setVisibility(View.GONE);
+
+                            Intent intent = new Intent(LoginActivity.this, InicioActivity.class);
+                            startActivity(intent);
+                        }
+                        else {
+                            Toast.makeText(getApplicationContext(), "Error al iniciar sesión!", Toast.LENGTH_LONG).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
+    }
+
+    private void initializeUI() {
+        emailTV = findViewById(R.id.user_input);
+        passwordTV = findViewById(R.id.login_password_input);
+        loginBtn = findViewById(R.id.login_btn);
+        progressBar = findViewById(R.id.progressBar);
+    }
+
+}
